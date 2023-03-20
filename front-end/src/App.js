f78352575d40cded0dd56bbf3fef65d1e1292d84
송수현 <ssh1692@naver.com>
2023-03-20 17:37:51 +0900

    💄 style : Main, Board 페이지

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,7 +11,9 @@ import SymptomsPage from "./pages/main/SymptomsPage";
 import NotFound404 from "./pages/NotFound404";
 import SignupPage from "./pages/SignupPage";
 import Board from "./pages/main/BoardPage";
+import BoardHashTag from "./../src/components/board/BoardHashTag.jsx";
 import PrivateRoute from "./router/PrivateRoute";
+import PostBoard from "./components/board/PostBoard";
 function App() {
   return (
     <Routes>
@@ -22,7 +24,8 @@ function App() {
         <Route path="main" element={<MainPage />} />
         <Route path="mypage" element={<ProfilePage />} />
         <Route path="catlist" element={<CatPage />} />
-        <Route path="board" element={<Board />} />
+        <Route path="board" element={<PostBoard />} />
+        <Route path="board/hashTag" element={<BoardHashTag />} />
         <Route path="symptoms" element={<SymptomsPage />} />
         <Route path="map" element={<MapPage />} />
       </Route>
