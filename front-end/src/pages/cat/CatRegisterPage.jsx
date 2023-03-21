325bb382673533dda3e92156afc91da44b50d8d3
한원종 <gksdnjswhd10@naver.com>
2023-03-21 17:17:14 +0900

    :sparkles: feat : 고양이 등록 컴포넌트 수정 중

--- front-end/src/pages/cat/CatRegisterPage.jsx
+++ front-end/src/pages/cat/CatRegisterPage.jsx
@@ -1,12 +1,12 @@
 import React from "react"
-import CatRegister from "../../components/cat/CatRegister";
+import CatImageRegister from "../../components/cat/CatImageRegister";
 
 const CatRegisterPage = () => {
     return(
         <div className="flex justify-center items-center h-screen">
-            <CatRegister />
+            <CatImageRegister />
 
-            <div className="w-[335px] h-[41.79px]">
+            <button className="w-[335px] h-[41.79px]">
                 <div
                 className="w-[335px] h-[41.79px] absolute left-[28.5px] top-[482.5px] rounded-[10px] bg-[#f1ebeb]"
                 style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
@@ -14,8 +14,8 @@ const CatRegisterPage = () => {
                 <p className="w-[58px] h-[22px] absolute left-[174px] top-[496px] text-xl font-bold text-left text-black">
                     이름
                 </p>
-            </div>
-            <div className="w-[335px] h-[41.79px]">
+            </button>
+            <button className="w-[335px] h-[41.79px]">
                 <div
                     className="w-[335px] h-[42px] absolute left-7 top-[544px] rounded-[10px] bg-[#f1ebeb]"
                     style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
@@ -23,8 +23,8 @@ const CatRegisterPage = () => {
                 <p className="w-[58px] h-[22px] absolute left-[174px] top-[558px] text-xl font-bold text-left text-black">
                     성별
                 </p>
-                </div>
-            <div className="w-[335px] h-[41.79px]">
+            </button>
+            <button className="w-[335px] h-[41.79px]">
                 <div
                 className="w-[335px] h-[41.79px] absolute left-[27.5px] top-[605.71px] rounded-[10px] bg-[#f1ebeb]"
                 style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
@@ -32,8 +32,8 @@ const CatRegisterPage = () => {
                 <p className="w-[58px] h-[22px] absolute left-[173px] top-[619px] text-xl font-bold text-left text-black">
                     나이
                 </p>
-            </div>
-            <div className="w-[335px] h-[66px]">
+            </button>
+            <button className="w-[335px] h-[66px]">
                 <div
                 className="w-[335px] h-[66px] absolute left-[26.5px] top-[667.5px] rounded-[30px] bg-[#e87f7f]"
                 style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
@@ -41,7 +41,7 @@ const CatRegisterPage = () => {
                 <p className="w-[58px] h-[35px] absolute left-[172px] top-[690px] text-xl font-bold text-left text-white">
                     등록
                 </p>
-            </div>
+            </button>
             
         </div>
     )
