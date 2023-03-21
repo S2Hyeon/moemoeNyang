b127dd071645a75bcc63d33628bc7f67e5b70bb1
송수현 <ssh1692@naver.com>
2023-03-21 17:22:36 +0900

    💄 style : Symptoms Page #183

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,7 +6,7 @@ import LoginPage from "./pages/LoginPage";
 import CatPage from "./pages/main/CatPage";
 import MainPage from "./pages/main/MainPage";
 import MapPage from "./pages/main/MapPage";
-// import SymptomsPage from "./pages/main/SymptomsPage";
+import SymptomsPage from "./pages/symptom/SymptomsPage";
 import NotFound404 from "./pages/NotFound404";
 import SignupPage from "./pages/SignupPage";
 import BoardHashTag from "./../src/components/board/BoardHashTag.jsx";
@@ -25,7 +25,7 @@ function App() {
         <Route path="catlist" element={<CatPage />} />
         <Route path="board" element={<PostBoard />} />
         <Route path="board/hashTag" element={<BoardHashTag />} />
-        {/* <Route path="symptoms" element={<SymptomsPaPge />} /> */}
+        <Route path="symptoms" element={<SymptomsPage />} />
         <Route path="map" element={<MapPage />} />
       </Route>
       <Route path="/admin" element={<AdminPage />} />
