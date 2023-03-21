a904aac3f0ef44e649d267a26beac69b8430eb2f
zdsa43 <zdsa43@gmail.com>
2023-03-21 14:21:26 +0900

    🐛 fix : 고양이 최근 위치 조회 null반환 문제 해결

--- back-end/src/main/java/com/ssafy/moemoe/api/response/board/BoardSpotResp.java
+++ back-end/src/main/java/com/ssafy/moemoe/api/response/board/BoardSpotResp.java
@@ -1,9 +1,19 @@
 package com.ssafy.moemoe.api.response.board;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+
+@Getter
+@ToString
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class BoardSpotResp {
 
     private long board_id;
