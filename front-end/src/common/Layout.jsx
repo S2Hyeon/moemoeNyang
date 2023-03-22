9313a112c5ca256c7c9e2c6f9f48457520a9f5db
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2023-03-22 11:01:28 +0900

    :recycle : refactor : 레이아웃 탑 바텀 거리 조절

--- front-end/src/common/Layout.jsx
+++ front-end/src/common/Layout.jsx
@@ -6,19 +6,12 @@ import Header from "./header/Header";
 const Layout = () => {
   // 테일윈드 레이아웃 https://stackoverflow.com/questions/59812003/tailwindcss-fixed-sticky-footer-on-the-bottom
   return (
-<<<<<<< HEAD
     <div className="flex flex-col h-screen justify-between overflow-hidden">
       <Header className="w-screen" />
       <div className="mb-auto w-screen overflow-scroll">
         <Outlet />
       </div>
       <Navbar className="w-screen" />
-=======
-    <div className="h-max">
-      <Header className="absolute top-0" />
-      <Outlet />
-      <Navbar className="sticky bottom-0" />
->>>>>>> a188b11c075f52a7d79b9cdfcee36d22a2d8a5db
     </div>
   );
 };
