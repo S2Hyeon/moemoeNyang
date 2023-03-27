58361f8d2e418e3a6458c2bd47e5fe4994a485cd
한원종 <gksdnjswhd10@naver.com>
2023-03-27 12:44:35 +0900

    :sparkles: feat : 고양이 리스트 수정

--- front-end/src/services/index.tsx
+++ front-end/src/services/index.tsx
@@ -1,11 +1,4 @@
 import Member from "./member";
-<<<<<<< HEAD
-import Catlist from "./cat";
-
-const Services = {
-  Member,
-  Catlist,
-=======
 import Cats from "./cats"
 import Main from "./main"
 
@@ -13,7 +6,6 @@ const Services = {
   Member,
   Cats,
   Main,
->>>>>>> a30c208b19f03cdb0ed12dfc7a5716cbf59aea33
 };
 
 export default Services;
