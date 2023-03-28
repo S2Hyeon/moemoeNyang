340362a5ff9dab7b173ecb403a0d9522aa75566e
송수현 <ssh1692@naver.com>
2023-03-28 11:58:40 +0900

    ✨ feat : cat detail page api

--- front-end/src/components/common/KakaoMap.jsx
+++ front-end/src/components/common/KakaoMap.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 
-export default function KakaoMap() {
+export default function KakaoMap({ catInfo }) {
   let map;
   useEffect(() => {
     let container = document.getElementById("map");
     let options = {
-      center: new window.kakao.maps.LatLng(35.85133, 127.734086),
-      level: 13,
+      center: new window.kakao.maps.LatLng(catInfo.lat, catInfo.lng),
+      level: 5,
     };
     map = new window.kakao.maps.Map(container, options);
     console.log("loading kakaomap");
