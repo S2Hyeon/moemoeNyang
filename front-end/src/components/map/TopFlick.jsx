1536b4bddbe0f797c6f76683590e06872078ecde
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2023-03-28 11:00:19 +0900

    :sparkles: feat : 바텀시트 리덕스 적용 & 클릭 아웃사이드 로직 수정

--- front-end/src/components/map/TopFlick.jsx
+++ front-end/src/components/map/TopFlick.jsx
@@ -3,13 +3,21 @@ import "@egjs/react-flicking/dist/flicking.css";
 import { useEffect } from "react";
 import Panel from "../common/Panel";
 import ProfileRound from "./ProfileRound";
+import { shallowEqual, useDispatch } from "react-redux";
+import { setSelectedCat } from "../../store/mapSlice";
+import { typedUseSelector } from "../../store";
 
-export default function TopFlick({
-  setTriggered,
-  catList,
-  setSelectedCat,
-  selectedCat,
-}) {
+export default function TopFlick(
+  {
+    // setTriggered,
+    // catList,
+    // setSelectedCat,
+    // selectedCat,
+  },
+) {
+  const dispatch = useDispatch();
+  const catList = typedUseSelector((state) => state.map.catList, shallowEqual);
+
   return (
     <Flicking align="prev" circular={true}>
       {catList.map((element, i) => {
@@ -20,14 +28,14 @@ export default function TopFlick({
             <div
               className="text-center text-xs ml-3"
               onClick={() => {
-                setSelectedCat(element);
-                console.log(element);
+                dispatch(setSelectedCat(element));
+                // setSelectedCat(element);
               }}
             >
               <ProfileRound
-                setTriggered={setTriggered}
+                // setTriggered={setTriggered}
                 image={image}
-                selectedCat={selectedCat}
+                // selectedCat={selectedCat}
                 catId={catId}
               />
               <div>{name}</div>
