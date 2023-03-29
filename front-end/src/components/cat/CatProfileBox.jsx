20d8dd8668bc5352268b7f83e02e7b4ccc688ce2
한원종 <gksdnjswhd10@naver.com>
2023-03-29 17:51:38 +0900

    :hammer: fix : 고양이 리스트 페이지 리팩토링

--- front-end/src/components/cat/CatProfileBox.jsx
+++ front-end/src/components/cat/CatProfileBox.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD
 export default function CatProfileBox({ catInfo }) {
   return (
     <div className="flex items-center relative p-5">
@@ -20,36 +19,6 @@ export default function CatProfileBox({ catInfo }) {
       </div>
       <div className=""></div>
       <div className=""></div>
-=======
-
-
-export default function CatProfileBox() {
-
-  return (
-    <div className="flex items-center relative p-5">
-      <img
-        className="w-30 rounded-[64px]"
-        src=""
-        alt="고양이 이미지"
-      />
-      <div className="ml-16">
-        <div className="font-bold text-sm">냥냥이</div>
-        <div className="font-bold text-sm">2살</div>
-        <div className="font-bold text-sm">남</div>
-        <div className="font-bold text-sm">팔로워 : 3명</div>
-        <div className="font-bold text-sm">질병 없음</div>
-      </div>
-      <div 
-        className=""
-			>
-
-      </div>
-      <div
-        className=""
-      >
-
-      </div>
->>>>>>> af335cace52712f4d8c30d384a370293d84ac79b
     </div>
   );
 }
