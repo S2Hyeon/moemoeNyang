5865ee284d027211ac587d6b99711304b8110e09
노정환 <zdsa43@gmail.com>
2023-03-31 01:06:28 +0900

    ✨ feat : snake_case 요청을 camelCase로 매핑할 수 있게 해주는 환경설정

--- back-end/src/main/java/com/ssafy/moemoe/config/WebConfig.java
+++ back-end/src/main/java/com/ssafy/moemoe/config/WebConfig.java
@@ -1,9 +1,15 @@
 package com.ssafy.moemoe.config;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -17,5 +23,22 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600L);
+    }
+
+    /*
+    JSON 데이터가 snake_case로 요청이 들어왔을 때 Req의 변수가 camelCase인 것과 일치시켜주는 환경설정
+    ex) JSON - { "cat_id":1 }
+        CatReq 클래스의 필드 private long catId;
+        위 두 변수명이 갖지 않아도 일치시켜준다!
+     */
+    @Override
+    public void configureMessageConverters(List<org.springframework.http.converter.HttpMessageConverter<?>> converters) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+
+        converters.add(converter);
     }
 }
