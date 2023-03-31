d9fcee2ea1784bf392b1fd748905713d8beb5096
한원종 <gksdnjswhd10@naver.com>
2023-03-31 15:03:12 +0900

    :hammer: chore : 고양이 등록 컴포넌트 성별 버튼 수정 중

--- front-end/src/components/cat/CatGenderButton.jsx
+++ front-end/src/components/cat/CatGenderButton.jsx
@@ -1,62 +1,58 @@
 import { useState } from "react";
 
-// 추후 선택형으로 수정 예정
 function CatGenderButton() {
-  const [inputVisible, setInputVisible] = useState(false);
+  const [buttonGroupVisible, setButtonGroupVisible] = useState(false);
   const [gender, setGender] = useState("");
-  
+
   const handleClick = () => {
-    setInputVisible(true);
+    setButtonGroupVisible(true);
   };
-  
-  const handleInputChange = (e) => {
-    setGender(e.target.value);
+
+  const handleButtonGroupChange = (e) => {
+    setGender(e.target.getAttribute("gender"));
   };
-  
-  const handleInputBlur = () => {
-    setInputVisible(false);
+
+  const handleButtonGroupBlur = () => {
+    setButtonGroupVisible(false);
   };
-  
+
   return (
     <div className="mb-4" style={{ position: "relative", width: "335px", height: "41.79px" }}>
-      {inputVisible ? (
-        <input
-          type="text"
-          placeholder="이름을 입력해 주세요."
-          value={gender}
-          onChange={handleInputChange}
-          onBlur={handleInputBlur}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          style={{ position: "absolute", top: 0, left: 0 }}
-        />
+      {buttonGroupVisible ? (
+        <div
+          style={{ position: "absolute", width: "100%" }}
+          onChange={handleButtonGroupChange}
+          onBlur={handleButtonGroupBlur}
+        >
+          <div className="flex justify-center flex-wrap">
+            <div className="flex">
+              <button
+                className="text-base rounded-r-none active:bg-[#dbbaba] flex justify-center px-20 py-2.5 rounded-full font-bold cursor-pointer bg-[#f1ebeb] "
+                style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
+                gender="M"
+              >
+                <div className="flex leading-5">남</div>
+              </button>
+              <button
+                className="text-base rounded-l-none active:bg-[#dbbaba] flex justify-center px-20 py-2.5 rounded-full font-bold cursor-pointer bg-[#f1ebeb] "
+                style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
+                gender="F"
+              >
+                <div className="flex leading-5">여</div>
+              </button>
+            </div>
+          </div>
+        </div>
       ) : (
-    <div className="flex justify-center items-baseline flex-wrap">
-        <div className="flex ">
-            <button className="text-base  rounded-r-none  focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
-         
-        bg-gray-100 
-        
-        border duration-200 ease-in-out 
-        border-gray-600 transition">
-                <div className="flex leading-5">
-                    
-                    남아 </div>
-            </button>
-            <button className="text-base  rounded-l-none border-l-0  focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
-        hover:bg-teal-200  
-        bg-teal-100 
-        
-        border duration-200 ease-in-out 
-        border-teal-600 transition">
-                <div className="flex leading-5">
-                  여아
-                    
-                </div>
-            </button>
-        </div>
-        </div>
+        <button
+          style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
+          className="rounded-full w-full h-full bg-[#f1ebeb] px-5 py-3 text-base mb-3 font-medium text-black transition duration-200 active:bg-[#e2d5d5]"
+          onClick={handleClick}
+        >
+          {gender === "M" ? "남" : gender === "F" ? "여" : "성별"}
+        </button>
       )}
-      </div>
+    </div>
   );
 }
 
