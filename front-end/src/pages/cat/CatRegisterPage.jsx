b00643ef4918225adf820d66337f7fc5b2b51075
한원종 <gksdnjswhd10@naver.com>
2023-03-31 17:45:54 +0900

    :hammer: chore : 고양이 등록 컴포넌트 성별 버튼 수정 중

--- front-end/src/pages/cat/CatRegisterPage.jsx
+++ front-end/src/pages/cat/CatRegisterPage.jsx
@@ -3,16 +3,14 @@ import CatImageRegister from "../../components/cat/CatImageRegister";
 import CatNameButton from "../../components/cat/CatNameButton";
 import CatGenderButton from "../../components/cat/CatGenderButton";
 import CatAgeButton from "../../components/cat/CatAgeButton";
+import CatRegisterButton from "../../components/cat/CatRegisterButton";
 // import ImgUpload from "../../components/upload/ImgUpload";
-// import CatNameInput from "../../components/cat/CatNameInput";
-// import CatGenderInput from "../../components/cat/CatGenderInput";
-// import CatAgeInput from "../../components/cat/CatAgeInput";
 
 
+export default function CatRegisterPage() {
 
-const CatRegisterPage = () => {
+
     return(
-        // <div className="ml-4 mr-4">
         <div className="flex justify-center items-center">
             <div className="w-auto grid gap-4 py-4">
                 {/* <ImgUpload /> */}
@@ -21,21 +19,10 @@ const CatRegisterPage = () => {
                     <CatNameButton />
                     <CatGenderButton />
                     <CatAgeButton />
-                    {/* <CatNameInput /> */}
-                    {/* <CatGenderInput /> */}
-                    {/* <CatAgeInput /> */}
-                    <button 
-                        className="flex justify-center items-center w-[335px] h-[66px] rounded-[30px] bg-[#e87f7f]"
-                        style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
-                        >
-                        <p className="flex justify-center w-[58px] h-[35px] left-[172px] top-[690px] text-xl font-bold text-left text-white">
-                        등록
-                        </p>
-                    </button>
+                    <CatRegisterButton />
+
                 </div>
             </div>
         </div>
     )
 }
-
-export default CatRegisterPage;
