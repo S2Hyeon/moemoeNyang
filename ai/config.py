932d435ed0b034e34b1abe887aab53d12d59160f
slhyj95 <slhyj95@naver.com>
2023-04-04 14:36:19 +0900

    :sparkles: feat : "인공지능 플라스크 프로젝트 업로드"

---
+++ ai/config.py
@@ -0,0 +1,7 @@
+TEST_IMAGE_SIZE = 224
+TEST_LEARNING_RATE = 0.000213388
+LOSS = 'categorical_crossentropy'
+
+class_labels = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7']
+
+LEARNING_RATE = [0.00049974, 0.00065424, 0.00012834, 0.00042999, 0.00070209, 4.0213e-05]
