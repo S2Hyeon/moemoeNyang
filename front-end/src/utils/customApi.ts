0e1d0eef137d842cf2eb408b9d451556d03458ce
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2023-04-05 09:56:36 +0900

    fix :getCookie 에러 수정

--- front-end/src/utils/customApi.ts
+++ front-end/src/utils/customApi.ts
@@ -27,15 +27,18 @@ Api.interceptors.request.use(
 
 Api.interceptors.response.use(
   (response) => {
+    console.log(response);
     return response;
   },
   (error) => {
-    const status = error.response.status;
-    if (status < 400 || status > 500) alert("알 수 없는 오류가 발생했습니다");
-    else if (status === 500) alert("서버에서 오류가 발생했습니다");
-    if (error.response && error.response.data)
-      return Promise.reject(error.response.data);
-    else return Promise.reject(error.message);
+    // const status = error.response.status || error.response;
+    console.log(error);
+    // if (status < 400 || status > 500) alert("알 수 없는 오류가 발생했습니다");
+    // else if (status === 500) alert("서버에서 오류가 발생했습니다");
+    // if (error.response && error.response.data)
+    //   return Promise.reject(error.response.data);
+    // else return Promise.reject(error.message);
+    return error;
   },
 );
 export default Api;
