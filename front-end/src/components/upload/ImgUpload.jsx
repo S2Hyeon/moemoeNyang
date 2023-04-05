2ca23417c9e3ede524f68ed5b3e70ac977729385
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2023-04-06 01:43:21 +0900

    ✨ feat : 게시판 사진 업로드 구현

--- front-end/src/components/upload/ImgUpload.jsx
+++ front-end/src/components/upload/ImgUpload.jsx
@@ -1,22 +1,34 @@
 import React, { useState, useRef } from "react";
 import { AiFillCamera } from "@react-icons/all-files/ai/AiFillCamera";
 
-export default function ImageUpload() {
+export default function ImageUpload({setImage}) {
   const [imgFile, setImgFile] = useState("");
   const imgRef = useRef();
 
   // 이미지 업로드 input의 onChange
-  const saveImgFile = () => {
+  const saveImgFile = (e) => {
+    if(!e.target.files[0]) return; //이미지 업로드를 취소하면 반환
     const file = imgRef.current.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onloadend = () => {
       setImgFile(reader.result);
+      console.log("image upload", e.target.files[0]);
+      setImage(e.target.files[0])
+      // console.log("image upload", e.target.files[0]);
+      // formData.append("catId", 2);
+      // formData.append("universityId", 1);
+      // formData.append("lng", 36.234);
+      // formData.append("content", "컨텐트내용");
+      // formData.append("image", e.target.files[0]);
+      // formData.append("lat", 12.123);
+      // for (const keyValue of formData) console.log(keyValue);
+      // setData(formData);
     };
   };
 
   return (
-    <form className="mt-4 ">
+    <form className="mt-4 " encType="multipart/form-data">
       <label className="imgUpload" htmlFor="imgUploadInput">
         {imgFile === "" ? (
           <div className="flex justify-center items-center w-full h-80 rounded-xl bg-slate-200">
